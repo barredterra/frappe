4149f086bf223fbe515b4739f53893b03ef79a43
barredterra <14891507+barredterra@users.noreply.github.com>
2024-02-11 18:10:46 +0100

    fix: skip redis

--- .github/helper/update_pot_file.sh
+++ .github/helper/update_pot_file.sh
@@ -5,7 +5,7 @@ cd ~ || exit
 echo "Setting Up Bench..."
 
 pip install frappe-bench
-bench -v init frappe-bench --skip-assets --python "$(which python)" --frappe-path "${GITHUB_WORKSPACE}"
+bench -v init frappe-bench --skip-assets --skip-redis-config-generation --python "$(which python)" --frappe-path "${GITHUB_WORKSPACE}"
 cd ./frappe-bench || exit
 
 echo "Generating POT file..."
